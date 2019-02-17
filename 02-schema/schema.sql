1b70c68aa3cd2220c08ddf0b64848e6ef78719c6
jingci_wang <imjingci@gmail.com>
2019-02-17 12:09:38 -0500

    Update schema.sql

--- 02-schema/schema.sql
+++ 02-schema/schema.sql
@@ -4,37 +4,40 @@ CREATE SCHEMA IF NOT EXISTS  repository  CHARACTER SET utf8mb4 COLLATE utf8mb4_u
 
 CREATE TABLE IF NOT EXISTS repository.repo (
 id      INTEGER     PRIMARY KEY, 
-owner    VARCHAR(100)    NOT NULL, 
-project   VARCHAR(100)    NOT NULL
+owner   VARCHAR(100)    NOT NULL, 
+project   VARCHAR(100)   NOT NULL
 );
 
+
+
 CREATE TABLE IF NOT EXISTS   repository.user (
+id                  INTEGER   NOT NULL,
 name            VARCHAR(100)      NOT NULL, 
 email             VARCHAR(100)     NOT NULL, 
-PRIMARY KEY (name, email)
+PRIMARY KEY (id)
 ) ; 
 
 CREATE TABLE IF NOT EXISTS   repository.commit (
-hash                    CHAR(40)         PRIMARY KEY, 
-subject                 VARCHAR (255)        NOT NULL, 
-message             VARCHAR(255)          NULL, 
+hash                   CHAR(40)       PRIMARY KEY, 
+subject                 VARCHAR (255)         NOT NULL, 
+message             VARCHAR (255)         NULL, 
 author_date         DATETIME         NOT NULL, 
-committer_date    DATETIME         NOT NULL, 
-author_name        VARCHAR(100)         NOT NULL, 
-author_email        VARCHAR(100)         NOT NULL, 
-committer_name    VARCHAR(100)       NOT NULL, 
-committer_email     VARCHAR(100)       NOT NULL, 
+committer_date    DATETIME        NOT NULL, 
+author_id              INTEGER     NOT NULL,
+committer_id          INTEGER     NOT NULL,
 repo_id                   INTEGER    NOT NULL, 
-FOREIGN KEY (author_name, author_email)  REFERENCES repository.user (name, email), 
-FOREIGN KEY (committer_name, committer_email) REFERENCES repository.user (name, email),
-FOREIGN KEY (repo_id)  REFERENCES repo (id)
+FOREIGN KEY (author_id)  REFERENCES repository.user (id), 
+FOREIGN KEY (committer_id) REFERENCES repository.user (id),
+FOREIGN KEY (repo_id)  REFERENCES repository.repo (id)
 ) ;
 
+
+
 CREATE TABLE IF NOT EXISTS  repository.modification (
-id         INTEGER       AUTO_INCREMENT, 
 number_deleted    INTEGER     NOT NULL, 
-number_added          INTEGER    NOT NULL,
-hash        CHAR      NOT NULL, 
- PRIMARY KEY (id), 
-FOREIGN KEY (hash)  REFERENCES  repository.commit (hash)
+number_add          INTEGER    NOT NULL,
+file_path                 CHAR(255)         NOT NULL,
+hash                      CHAR(40)     NOT NULL, 
+FOREIGN KEY (hash)  REFERENCES  repository.commit (hash),
+PRIMARY KEY (hash, file_path)
 );
