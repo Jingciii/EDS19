0a582a73fd6e921626883fa4dba88b695be7f005
jingci_wang <imjingci@gmail.com>
2019-02-19 12:37:31 -0500

    Update schema

--- 02-schema/schema.sql
+++ 02-schema/schema.sql
@@ -13,16 +13,16 @@ project   VARCHAR(100)   NOT NULL
 CREATE TABLE IF NOT EXISTS   repository.user (
 id                  INTEGER   NOT NULL,
 name            VARCHAR(100)      NOT NULL, 
-email             VARCHAR(100)     NOT NULL, 
+email             VARCHAR(100)      NULL, 
 PRIMARY KEY (id)
 ) ; 
 
 CREATE TABLE IF NOT EXISTS   repository.commit (
 hash                   CHAR(40)       PRIMARY KEY, 
-subject                 VARCHAR (255)         NOT NULL, 
+subject                 VARCHAR (255)         NULL, 
 message             VARCHAR (255)         NULL, 
-author_date         DATETIME         NOT NULL, 
-committer_date    DATETIME        NOT NULL, 
+author_date         DATETIME          NULL, 
+committer_date    DATETIME         NULL, 
 author_id              INTEGER     NOT NULL,
 committer_id          INTEGER     NOT NULL,
 repo_id                   INTEGER    NOT NULL, 
@@ -35,7 +35,7 @@ FOREIGN KEY (repo_id)  REFERENCES repository.repo (id)
 
 CREATE TABLE IF NOT EXISTS  repository.modification (
 number_deleted    INTEGER     NOT NULL, 
-number_add          INTEGER    NOT NULL,
+number_added          INTEGER    NOT NULL,
 file_path                 CHAR(255)         NOT NULL,
 hash                      CHAR(40)     NOT NULL, 
 FOREIGN KEY (hash)  REFERENCES  repository.commit (hash),
